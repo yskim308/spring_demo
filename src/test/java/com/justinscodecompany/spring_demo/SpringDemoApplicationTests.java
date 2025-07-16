c801e2b126f58dddcee7507c6dd03c70ae73e9a6
vantaluex <vantaluex@gmail.com>
2025-07-16 16:24:54 +0900

    made a new jumpstart file

---
+++ src/test/java/com/justinscodecompany/spring_demo/SpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.justinscodecompany.spring_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
